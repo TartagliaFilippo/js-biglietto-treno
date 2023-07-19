162309089410438097cbe686b8525f4b40abc4ba
filippo199600 <pippotartaglia@gmail.com>
2023-07-19 15:33:17 +0200

    JS add prompts

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const tripLength = prompt("quanti chilometri desideri percorrere in treno?");
+console.log("lunghezza viaggio: " + tripLength);
+const passengerAge = prompt("quanti anni hai?");
+console.log("età passeggero: " + passengerAge);
